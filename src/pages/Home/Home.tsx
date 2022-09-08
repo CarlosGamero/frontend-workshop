6380df95d023b9f1edd405adc70d9c72be2986b8
CarlosGamero <carlos.gamero@lokalise.com>
2022-09-08 12:37:02 +0200

    Folder structure changes

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Container} from './styles'
-import TooltipComponent from './Components/TooltipComponent'
+import TooltipComponent from './Components/TooltipComponent/TooltipComponent'
 
 const Home = () => {
   return (
