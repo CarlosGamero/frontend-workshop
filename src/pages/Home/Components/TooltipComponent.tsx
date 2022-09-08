7d10b4b1b586a31f30fce718ca452d91f5182d58
CarlosGamero <carlos.gamero@lokalise.com>
2022-09-08 12:32:51 +0200

    tooltip exercise

---
+++ src/pages/Home/Components/TooltipComponent.tsx
@@ -0,0 +1,10 @@
+import {Tooltip} from "@lokalise/louis";
+
+const TooltipComponent = () => {
+
+    return (
+        <Tooltip popoverContent="I am backend engineer :')">My name is Carlos</Tooltip>
+    )
+}
+
+export default TooltipComponent;
