b7000630c326e4dffa85e80aa479c4d7bc234d9c
CarlosGamero <carlos.gamero@lokalise.com>
2022-09-16 18:26:48 +0200

    error fix

--- src/components/TaskComponent/TaskComponent.tsx
+++ src/components/TaskComponent/TaskComponent.tsx
@@ -11,20 +11,15 @@ interface TaskComponentProps{
 export default class TaskComponent extends React.Component<TaskComponentProps>{
 
     private style: CSSProperties = {
-        backgroundColor: this.props.task.bgColor,
         width: '100%',
         padding: '20px 5px',
         boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)',
         borderRadius: 40
     }
 
-    shouldComponentUpdate(): boolean {
-        return false;
-    }
-
     render() {
         return (
-            <Flex justify={"center"} style={this.style}>
+            <Flex justify={"center"} style={{...this.style, backgroundColor: this.props.task.bgColor}}>
                 <TaskTitle> {this.props.task.title} </TaskTitle>
             </Flex>
         );
